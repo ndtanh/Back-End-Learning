9c129f5e36a7224f1c72e29b7d08537a581299c7
Tuan Anh <ndtanh.99@gmail.com>
2024-10-31 00:30:31 +0700

    Update Facade Design Pattern

---
+++ Design Pattern/Facade Design Pattern/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Facade_Design_Pattern;
+
+class Program
+{
+    static void Main()
+    {
+        OrderFacade orderFacade = new OrderFacade();
+        
+        // Place an order using the facade
+        int productId = 123;
+        string paymentInfo = "CreditCard";
+        string address = "123 Main St, Anytown, USA";
+        
+        orderFacade.PlaceOrder(productId, paymentInfo, address);
+    }
+}
